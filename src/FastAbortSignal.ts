e1a0a5b41ada34411ab9ca74195170b1d90ec365
Tejesh Mehta <tejesh.mehta@gmail.com>
2021-07-10 03:05:02 -0700

    use global AbortController if it exists, for best browser compat

---
+++ src/FastAbortSignal.ts
@@ -0,0 +1,51 @@
+import { AbortSignalType } from './AbortSignalType'
+import EventEmitter3 from 'eventemitter3'
+
+export default class AbortSignal implements AbortSignalType {
+  private ee = new EventEmitter3()
+  readonly aborted: boolean = false
+
+  static abort(): AbortSignalType {
+    const signal = new AbortSignal()
+    signal.dispatchEvent({ type: 'abort' } as Event)
+    return signal
+  }
+
+  set onabort(
+    listener: (this: AbortSignalType, ev: AbortSignalEventMap['abort']) => any,
+  ) {
+    this.addEventListener('abort', listener)
+  }
+  addEventListener<K extends keyof AbortSignalEventMap>(
+    type: K,
+    listener: (this: AbortSignal, ev: AbortSignalEventMap[K]) => any,
+    options?: boolean | AddEventListenerOptions,
+  ): void
+  addEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | AddEventListenerOptions,
+  ): void {
+    // @ts-ignore
+    this.ee.addListener(type, listener)
+  }
+  removeEventListener<K extends keyof AbortSignalEventMap>(
+    type: K,
+    listener: (this: AbortSignal, ev: AbortSignalEventMap[K]) => any,
+    options?: boolean | EventListenerOptions,
+  ): void
+  removeEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | EventListenerOptions,
+  ): void {
+    // @ts-ignore
+    this.ee.removeListener(type, listener)
+  }
+  dispatchEvent(event: Event): boolean {
+    // @ts-ignore
+    this.aborted = true
+    this.ee.emit(event.type)
+    return true
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,23 @@
-import { AbortSignalType } from './AbortSignalType'
-import EventEmitter3 from 'eventemitter3'
+import _FastAbortSignal from './FastAbortSignal'
+// @ts-ignore
+import getGlobalThis from 'globalthis'
 
-class AbortSignal implements AbortSignalType {
-  private ee = new EventEmitter3()
-  readonly aborted: boolean = false
+const globalThis = getGlobalThis()
 
-  static abort(): AbortSignalType {
-    const signal = new AbortSignal()
-    signal.dispatchEvent({ type: 'abort' } as Event)
-    return signal
-  }
+export const FastAbortSignal = _FastAbortSignal
 
-  set onabort(
-    listener: (this: AbortSignalType, ev: AbortSignalEventMap['abort']) => any,
-  ) {
-    this.addEventListener('abort', listener)
-  }
-  addEventListener<K extends keyof AbortSignalEventMap>(
-    type: K,
-    listener: (this: AbortSignal, ev: AbortSignalEventMap[K]) => any,
-    options?: boolean | AddEventListenerOptions,
-  ): void
-  addEventListener(
-    type: string,
-    listener: EventListenerOrEventListenerObject,
-    options?: boolean | AddEventListenerOptions,
-  ): void {
-    // @ts-ignore
-    this.ee.addListener(type, listener)
-  }
-  removeEventListener<K extends keyof AbortSignalEventMap>(
-    type: K,
-    listener: (this: AbortSignal, ev: AbortSignalEventMap[K]) => any,
-    options?: boolean | EventListenerOptions,
-  ): void
-  removeEventListener(
-    type: string,
-    listener: EventListenerOrEventListenerObject,
-    options?: boolean | EventListenerOptions,
-  ): void {
-    // @ts-ignore
-    this.ee.removeListener(type, listener)
-  }
-  dispatchEvent(event: Event): boolean {
-    // @ts-ignore
-    this.aborted = true
-    this.ee.emit(event.type)
-    return true
-  }
+class FastAbortController implements AbortController {
+  signal: AbortSignal = new FastAbortSignal()
+  abort = () => this.signal.dispatchEvent({ type: 'abort' } as Event)
 }
 
-export const FastAbortSignal = AbortSignal
+const hasAbortController =
+  typeof globalThis !== 'undefined' &&
+  'AbortController' in globalThis &&
+  globalThis.AbortController !== FastAbortController
 
-export default class FastAbortController implements AbortController {
-  signal = new AbortSignal()
-  abort() {
-    this.signal.dispatchEvent({ type: 'abort' } as Event)
-  }
-}
+const AbortControllerClass = hasAbortController
+  ? window.AbortController
+  : FastAbortController
+
+export default AbortControllerClass
