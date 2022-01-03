22196902c0e3e1958e420c5a17132eb442a49fa7
Tejesh Mehta <tejesh.mehta@gmail.com>
2022-01-03 15:48:51 -0800

    window fix

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ export const hasAbortController =
   globalThis.AbortController !== FastAbortController
 
 const AbortControllerClass = hasAbortController
-  ? window.AbortController
+  ? globalThis.AbortController
   : FastAbortController
 
 export default AbortControllerClass
